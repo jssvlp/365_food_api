cd1bfd87262a7a004018343c0f05780753175996
joshadow <jossias.vel@gmail.com>
2021-06-29 00:15:51 -0400

    connection with supabase api

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ClientService;
 use App\Services\FacturascriptService;
 use App\Services\OrderService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class OrderController extends Controller
 {
@@ -12,13 +14,39 @@ class OrderController extends Controller
     {
         $order = (new OrderService())->createInvoice($request);
 
+        $supabaseApiUrl = env('SUPABASE_API_CLIENT_URL') .'/orders';
+
+        $items = collect($request->items);
+
+        $items = $items->map( function ($item){
+            return [
+                'name' => $item['descripcion'],
+                'quantity' => $item['cantidad']
+            ];
+        });
+
+        $client = (new ClientService())->getClient($order->data['codcliente']);
+
+        $orderBody = [
+            "orderNumber"=> $order->data['codigo'],
+            "status"=> "Pendiente",
+            "client"=> $client->data['nombre'],
+            "phone"=> $client->data['telefono1'],
+            "address"=> $request->direccionorden,
+            "orderDetail"=> $items
+        ];
+
+
+        $supabaseApiResponse = Http::post($supabaseApiUrl,$orderBody);
+        $decoded = json_decode($supabaseApiResponse->getBody()->getContents());
+
+        //Create tracking in Supabase
+
         return response()->json([
             'success' => $order->success,
             'message' => $order->message,
             'data' => $order->data
         ]);
     }
-
-
 
 }
